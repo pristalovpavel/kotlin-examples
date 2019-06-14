7cf3469f6bc12fde23b25ff44164bc3af21e5722
pristalovpavel <pristalovpavel@gmail.com>
2019-06-14 16:57:57 +0300

    сделано приложение для iOS

---
+++ tutorials/mpp-iOS-Android/native/KotlinIOS/KotlinIOS/GameFieldCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GameFieldCollectionViewCell.swift
+//  KotlinIOS
+//
+//  Created by Pristalov Pavel on 14/06/2019.
+//  Copyright © 2019 Evgeny Petrenko. All rights reserved.
+//
+
+import UIKit
+
+class GameFieldCollectionViewCell: UICollectionViewCell {
+    
+}
--- tutorials/mpp-iOS-Android/native/KotlinIOS/KotlinIOS/ViewController.swift
+++ tutorials/mpp-iOS-Android/native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -1,33 +1,101 @@
-//
-//  ViewController.swift
-//  KotlinIOS
-//
-//  Created by Evgeny Petrenko on 26.09.18.
-//  Copyright © 2018 Evgeny Petrenko. All rights reserved.
-//
-
 import UIKit
 import main
 
+@objc class ViewController: UIViewController, GameEngineCallbacks {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var b11: UIButton!
+    @IBOutlet weak var b12: UIButton!
+    @IBOutlet weak var b13: UIButton!
+    @IBOutlet weak var b21: UIButton!
+    @IBOutlet weak var b22: UIButton!
+    @IBOutlet weak var b23: UIButton!
+    @IBOutlet weak var b31: UIButton!
+    @IBOutlet weak var b32: UIButton!
+    @IBOutlet weak var b33: UIButton!
+    
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    @IBOutlet weak var newGame: UIButton!
+    
+    func clearUIField() {
+        // buttons
+        let arr = [b11, b12, b13, b21, b22, b23, b31, b32, b33]
+        for elem in arr {
+            elem?.setTitle("_", for: .normal)
+        }
+        winnerLabel.text = ""
+    }
+    
+    func showZero(i: Int32, j: Int32) {
+        // buttons
+        let arr = [[b11, b12, b13], [b21, b22, b23], [b31, b32, b33]]
+        arr[Int(i)][Int(j)]?.setTitle("o", for: .normal)
+    }
+    
+    func showWinner(message: String) {
+        winnerLabel.text = message
+    }
+    private var engine: GameEngine?
 
-
-
-
-class ViewController: UIViewController {
-
+    // Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
-        label.center = CGPoint(x: 160, y: 285)
-        label.textAlignment = .center
-        label.font = label.font.withSize(25)
-        label.text = CommonKt.createApplicationScreenMessage()
-        view.addSubview(label)
-        
-        
+        engine = GameEngine(callbacks: self)
+
+        titleLabel.text = CommonKt.createApplicationScreenMessage()
+        engine?.startNewGame()
     }
 
+    // Actions
+    @IBAction func onNewGameClick(_ sender: Any) {
+        engine?.startNewGame()
+    }
 
+    @IBAction func onB11Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 0, j: 0)
+    }
+
+    @IBAction func onB12Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 0, j: 1)
+    }
+
+    @IBAction func onB13Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 0, j: 2)
+    }
+
+    @IBAction func onB21Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 1, j: 0)
+    }
+
+    @IBAction func onB22Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 1, j: 1)
+    }
+
+    @IBAction func onB23Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 1, j: 2)
+    }
+
+    @IBAction func onB31Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 2, j: 0)
+    }
+
+    @IBAction func onB32Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 2, j: 1)
+    }
+
+    @IBAction func onB33Click(_ sender: UIButton) {
+        sender.setTitle("x", for: .normal)
+        engine?.fieldPressed(i: 2, j: 2)
+    }
 }
 
